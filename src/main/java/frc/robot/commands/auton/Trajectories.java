901a330406e36e3c46c9314581c218975471eed1
25ChilingH <25ChilingH@students.harker.org>
2022-08-30 21:55:20 -0700

    Fix trajectories

--- src/main/java/frc/robot/commands/auton/Trajectories.java
+++ src/main/java/frc/robot/commands/auton/Trajectories.java
@@ -16,18 +16,18 @@ public class Trajectories {
   public static final Pose2d[] hAG = {
     new Pose2d(new Translation2d(6.8, 5.63), Rotation2d.fromDegrees(-65.42)),
     new Pose2d(new Translation2d(6.28, 6.79), Rotation2d.fromDegrees(-65.42)),
-    new Pose2d(new Translation2d(1.67, 6.56), Rotation2d.fromDegrees(180)),
-    new Pose2d(new Translation2d(1.85, 4.41), Rotation2d.fromDegrees(180)),
-    new Pose2d(new Translation2d(4.03, 3.29), Rotation2d.fromDegrees(180)),
+    new Pose2d(new Translation2d(1.67, 6.56), Rotation2d.fromDegrees(-90.0)),
+    new Pose2d(new Translation2d(1.85, 4.41), Rotation2d.fromDegrees(180.0)),
+    new Pose2d(new Translation2d(4.03, 3.29), Rotation2d.fromDegrees(135.59)),
     new Pose2d(new Translation2d(5.12, 3.64), Rotation2d.fromDegrees(-167.83))
   };
   public static final Pose2d[] hFFB = {
     new Pose2d(new Translation2d(6.18, 3.99), Rotation2d.fromDegrees(26.92)),
     new Pose2d(new Translation2d(5.02, 3.47), Rotation2d.fromDegrees(20.20)),
-    new Pose2d(new Translation2d(1.82, 4.44), Rotation2d.fromDegrees(0)),
-    new Pose2d(new Translation2d(2.22, 6.49), Rotation2d.fromDegrees(90)),
-    new Pose2d(new Translation2d(5.54, 7.4), Rotation2d.fromDegrees(-13.73)),
-    new Pose2d(new Translation2d(6.41, 5.82), Rotation2d.fromDegrees(51.71))
+    new Pose2d(new Translation2d(1.82, 4.44), Rotation2d.fromDegrees(0.0)),
+    new Pose2d(new Translation2d(2.22, 6.49), Rotation2d.fromDegrees(-90.0)),
+    new Pose2d(new Translation2d(5.54, 7.4), Rotation2d.fromDegrees(166.27)),
+    new Pose2d(new Translation2d(6.41, 5.82), Rotation2d.fromDegrees(128.29))
   };
 
   public static final Trajectory hoarderAgainstGremlin = generateTrajectory(hAG, defaultConfig);
